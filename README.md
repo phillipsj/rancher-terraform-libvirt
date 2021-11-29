5d0859589d6c6005c01e1f7b3de2d21245229f27
Jamie Phillips <jamie.phillips@suse.com>
2021-11-29 14:53:13 -0500

    Basic setup for a Rancher server.
    Signed-off-by: Jamie Phillips <jamie.phillips@suse.com>

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 rancher-terraform-libvirt
+
+
+Set the environment variable, the current settings would be for your local libvirt. You can do a remote one, but you will
+need to ensure you have a propery   
+
+```Bash
+LIBVIRT_DEFAULT_URI=qemu:///system
+```
+
---
+++ main.tf
@@ -0,0 +1,75 @@
+terraform {
+  required_providers {
+    libvirt = {
+      source = "dmacvicar/libvirt"
+    }
+  }
+}
+
+provider "libvirt" {
+  # Configuration options
+}
+
+resource "libvirt_volume" "tumbleweed" {
+  name   = "tumbleweed"
+  pool   = "default"
+  source = "http://download.opensuse.org/tumbleweed/appliances/openSUSE-Tumbleweed-JeOS.x86_64-OpenStack-Cloud.qcow2"
+  format = "qcow2"
+}
+
+data "template_file" "user_data" {
+  template = file("${path.module}/cloud_init.cfg")
+}
+
+data "template_file" "network_config" {
+  template = file("${path.module}/network_config.cfg")
+}
+
+# for more info about paramater check this out
+# https://github.com/dmacvicar/terraform-provider-libvirt/blob/master/website/docs/r/cloudinit.html.markdown
+# Use CloudInit to add our ssh-key to the instance
+# you can add also meta_data field
+resource "libvirt_cloudinit_disk" "commoninit" {
+  name           = "commoninit.iso"
+  user_data      = data.template_file.user_data.rendered
+  network_config = data.template_file.network_config.rendered
+}
+
+# Create the machine
+resource "libvirt_domain" "rancher" {
+  name   = "tumbleweed-terraform"
+  memory = "512"
+  vcpu   = 1
+
+
+  cloudinit = libvirt_cloudinit_disk.commoninit.id
+
+  network_interface {
+    network_name = "default"
+  }
+
+  # IMPORTANT: this is a known bug on cloud images, since they expect a console
+  # we need to pass it
+  # https://bugs.launchpad.net/cloud-images/+bug/1573095
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = libvirt_volume.tumbleweed.id
+  }
+
+  graphics {
+    type        = "spice"
+    listen_type = "address"
+    autoport    = true
+  }
+}
